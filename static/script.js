447c2146c8e809562e21cb7415776b8f363ba315
Adrian Gallano <adriangallanoengr@gmail.com>
2023-02-01 13:37:03 +0800

    Add ui optimization

--- static/script.js
+++ static/script.js
@@ -1,31 +1,40 @@
 const keypad = document.getElementById("keypad")
-const calculatorPattern = /[0-9\+\-\*\/]/;
+const calculatorPattern = /[0-9\+\-\*\/\.\%]/;
 let screenState = document.querySelector(".screen > p");
+let numState;
+
+const operation = {
+	"Backspace": function () {
+		screenState.textContent = screenState.textContent.slice(0, screenState.textContent.length - 1)
+	},
+	"Clear": function () {
+		screenState.textContent = ""
+	},
+
+}
 
 keypad.addEventListener("click", (e) => {
 	displayScreen(e.target.dataset.num)
 })
 
 document.addEventListener("keydown", (e) => {
-	if(!e.key.match(calculatorPattern) && e.key != "Backspace") return;
+	if (!e.key.match(calculatorPattern) && e.key != "Backspace") return;
 	displayScreen(e.key)
 })
 
+function displayScreen(buttonPressed) {
+		if (buttonPressed.match(/[0-9]/)) { // if numbers
 
-function displayScreen(buttonPressed){
-	if (buttonPressed.match(/[0-9]/)) { // if numbers
-		screenState.textContent += buttonPressed
-	}else{ /* if operations */
+			if (screenState.textContent.length >= 17) return;
 
-		if (buttonPressed == "Backspace") { // if backspace
-			screenState.textContent = screenState.textContent.slice(0, screenState.textContent.length - 1)
+			screenState.textContent += buttonPressed
+
+		} else { /* if operations */
+
+			operation[buttonPressed]()
 		}
-	
-		else if (buttonPressed == "clear") { // clear
-			screenState.textContent = ""
-		}
-	}
 
-
+	numState = Number(screenState.textContent)
+	console.log(numState)
 }
 
