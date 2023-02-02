b9ac919d88024620778d17b7110ed1e75883cb3e
Adrian Gallano <adriangallanoengr@gmail.com>
2023-02-02 13:45:03 +0800

    Add full features

--- static/script.js
+++ static/script.js
@@ -1,11 +1,15 @@
 const keypad = document.getElementById("keypad")
-const calculatorPattern = /[0-9\+\-\*\/\.\%]/;
+const calculatorPattern = /[0-9\+\-\*\/\.\%\=]/;
 let screenState = document.querySelector(".screen > p");
+let pastScreenState = document.querySelector(".past-screen > p")
+let pastOperator;
 let numState;
+let pastState = 0;
 
 const operation = {
 	"Backspace": function () {
-		if (screenState.textContent.toLowerCase().includes("e") && screenState.textContent.length > 3) {
+		if (screenState.textContent.toLowerCase().includes("e") && screenState.textContent.length > 4
+		) {
 			screenState.textContent = screenState.textContent
 				.slice(0, screenState.textContent.indexOf("e") - 1) + screenState.textContent
 					.slice(screenState.textContent.indexOf("e"))
@@ -18,6 +22,8 @@ const operation = {
 	},
 	"Clear": function () {
 		screenState.textContent = ""
+		pastScreenState.textContent = ""
+		pastState = 0
 	},
 	"Change-sign": function () {
 		if (screenState.textContent.includes("-")) {
@@ -35,8 +41,28 @@ const operation = {
 		if (screenState.textContent.includes("%") && screenState.textContent.length <= 16) return;
 
 		screenState.textContent = Number(screenState.textContent) / 100
-	}
 
+	},
+	"+": function () {
+		mathFunc("+")
+	},
+	"-": function () {
+		mathFunc("-")
+	},
+	"/": function () {
+		mathFunc("/")
+	},
+	"*": function () {
+		mathFunc("*")
+	},
+	"=": function () {
+		if(screenState.textContent.length <= 0){
+			pastScreenState.textContent = ""
+			screenState.textContent = pastState
+		}else{
+			mathFunc(pastOperator)
+		}
+	}
 }
 
 keypad.addEventListener("click", (e) => {
@@ -63,6 +89,43 @@ function displayScreen(buttonPressed) {
 		screenState.textContent = Number(screenState.textContent).toExponential();
 	}
 	numState = Number(screenState.textContent)
-	console.log(numState)
 }
+
+function mathFunc(operator) {
+	if(screenState.textContent.length <= 0){
+		pastOperator = operator
+		pastScreenState.textContent = `${pastState} ${pastOperator}`
+		return;
+	}
+
+	if(pastScreenState.textContent.length <= 0){
+		pastState = numState
+		changeScreen(operator)
+	}else{
+		switch (pastOperator) {
+			case "+":
+				pastState += numState
+				break;
+			case "-":
+				pastState -= numState
+				break;
+			case "/":
+				pastState /= numState
+				break;
+			case "*":
+				pastState *= numState
+				break;
+			default:
+				return "Math error"
+		}
+		changeScreen(operator)
+	}
+}
+
+function changeScreen(operator){
+	pastOperator = operator
+	pastScreenState.textContent = `${pastState} ${pastOperator}`
+	screenState.textContent = ""
+}
+
 
